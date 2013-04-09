86ec040d0b9cd1298b4af4cd221856ca5baf738c
unknown <jrm25881@anct234user15.nvstu.edu>
2013-04-09 10:47:38 -0400

    Finished Notes Completed Assignment 4

--- intel/assignment4.3/proj4/proj4/p4.asm
+++ intel/assignment4.3/proj4/proj4/p4.asm
@@ -3,32 +3,60 @@
 .stack 2048
 
 .data
-num		sword	100,-100,200,300
-maxi	sword	?
+input byte "IV"
+input2 dword 2 dup (?)
+
 
 ExitProcess	proto, exitcode:dword
 
 .code
 start:
-mov	esi,offset num+2
-mov ecx,(lengthof num) - 1 
-mov	ax,num
-next:
+mov ecx,lengthof input
+mov edi,offset input
+mov esi,offset input2
 
-cmp	ax,[esi]
-jl	smaller
+transitionloop:
+	mov al,[edi]
+	.if al=='M'
+		mov eax,1000
+	.elseif al=='D'
+		mov eax,500
+	.elseif al=='C'
+		mov eax,100
+	.elseif al=='L'
+		mov eax,50
+	.elseif al=='X'
+		mov eax,10
+	.elseif al=='V'
+		mov eax,5
+	.elseif al=='I'
+		mov eax,1
+	.else	
+		mov eax,0
+	.endif
+	mov [esi],eax	
+	add edi, type input
+	add esi, type input2
+loop transitionloop
 
-jmp	continue
+mov ecx,lengthof input2
+sub esi,type input2
+mov eax,[esi]
+mov ebx,eax
+sub esi,type input2
+dec ecx
 
-smaller:
-mov	ax,[esi]
+additionloop:
+	mov edx,[esi]
+	.if edx >= ebx
+		mov ebx,edx
+		add eax,edx
+	.else
+		sub eax,edx
+	.endif
+	sub esi,type input2
+loop additionloop
 
-continue:
-add	esi,type num
-
-loop	next
-
-mov	maxi,ax
 invoke	ExitProcess, 0
 end start
 
